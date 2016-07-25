4ad2d6cd4155e5e1390fe287b2d27a28edb31c02
Anna Matviyenko <anna.matviyenko@gmail.com>
2016-07-24 22:52:43 -0700

    Add backend logic to js file

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,28 @@ var pizzaPrice = 0;
   };
 
 
+// Frontend logic
+$(document).ready(function() {
+  $("form#pizzaOrder").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("input#new-name").val();
+    var checkedToppings= [];
+    $(":checkbox:checked").each(function(topping){
+      checkedToppings[topping] = $(this).val();
+    });
+    var selectedSize= $("select#new-size").val();
+    var inputtedQuantity= parseInt($("input#new-quantity").val());
+    var newPizza= new Pizza(inputtedName,checkedToppings ,selectedSize,inputtedQuantity);
+    newPizza.totalPrice= newPizza.price();
+
+
+    $("#pizza").show();
+    $(".name").text(newPizza.name);
+    $(".toppings").text(newPizza.toppings);
+    $(".size").text(newPizza.size);
+    $(".quantity").text(newPizza.quantity);
+    $(".totalPrice").text(newPizza.totalPrice);
+
+});
+});
